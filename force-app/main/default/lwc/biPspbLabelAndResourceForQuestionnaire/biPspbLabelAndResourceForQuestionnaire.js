c91c68f4188f5167bb4c0ec6755170b6bc6892a4
nagsvalheal <158153501+nagsvalheal@users.noreply.github.com>
2024-08-05 10:28:27 +0530

    05081030 Commit

---
+++ force-app/main/default/lwc/biPspbLabelAndResourceForQuestionnaire/biPspbLabelAndResourceForQuestionnaire.js
@@ -0,0 +1,185 @@
+// Custom labels
+export { default as HEADING_TEXT } from '@salesforce/label/c.BI_PSP_PsoriasisLabel';
+export { default as HEADING_TEXT_TWO } from '@salesforce/label/c.BI_PSP_PsoriasisLabelOne';
+export { default as EXPIRE_LABEL } from '@salesforce/label/c.BI_PSP_ExpireInLabel';
+export { default as PSS_CATEGORY } from '@salesforce/label/c.BI_PSP_PssCategory';
+export { default as ROLLOUT_DATE_TEXT } from '@salesforce/label/c.BI_PSP_RolloutDateForCard';
+export { default as ANS_TEXT } from '@salesforce/label/c.BI_PSP_AnsweredLabel';
+export { default as BRANDED_URL } from '@salesforce/label/c.BI_PSPB_SiteLabel';
+export { default as BRANDED_NAVI_URL } from '@salesforce/label/c.BI_PSPB_BrandedSiteNaviUrl';
+export { default as UN_ASSIGNED_URL } from '@salesforce/label/c.BI_PSPB_UnAssignedLabel';
+ export { default as UN_ASSIGNED_URL_NAVI } from '@salesforce/label/c.BI_PSPB_UnAssignedNaviUrl';
+export { default as COMPLETED } from '@salesforce/label/c.BI_PSP_CompletedOn';
+export { default as CONSOLE_ERROR_MESSAGE } from '@salesforce/label/c.BI_PSP_ConsoleError';
+export { default as PSS_QUESTIONNAIRE_URL } from '@salesforce/label/c.BI_PSPB_PsoriasisQuesUrl';
+export { default as PSS_COMPLETED_QUESTIONNAIRE_URL } from '@salesforce/label/c.BI_PSPB_PsoriasisCompletedQuesUrl';
+export { default as ERROR_VARIANT } from '@salesforce/label/c.BI_PSP_ErrorVariantToast';
+export { default as COMPLETED_LABEL } from '@salesforce/label/c.BI_PSP_Completed';
+export { default as IN_PROGRESS } from '@salesforce/label/c.BI_PSP_InProgressTxt';
+export { default as RESUME } from '@salesforce/label/c.BI_PSP_ResumeTxt';
+export { default as EXPIRED } from '@salesforce/label/c.BI_PSP_Expired';
+export { default as START_LABEL } from '@salesforce/label/c.BI_PSP_StartLabel';
+
+export { default as INTRODUCTION_CATEGORY } from '@salesforce/label/c.BI_PSP_IntroductionTxt';
+export { default as LETS_PERSONLIZETEXT } from '@salesforce/label/c.BI_PSP_PersonlizeHeading';
+export { default as ANS_LABEL } from '@salesforce/label/c.BI_PSP_AnwerLabel';
+export { default as ANS_LABEL_ONE } from '@salesforce/label/c.BI_PSP_LetsPersonlizeMessage';
+export { default as ANS_LABEL_TWO } from '@salesforce/label/c.BI_PSP_LetsPersonlizeMsgOne';
+export { default as ANS_LABEL_THREE } from '@salesforce/label/c.BI_PSP_LetsPersonlizeMsgTwo';
+export { default as ANS_LABEL_FOUR } from '@salesforce/label/c.BI_PSP_LetsPersonlizeMsgThree';
+export { default as ANS_LABEL_Five } from '@salesforce/label/c.BI_PSP_AnsweredLabel';
+export { default as YES_LABEL } from '@salesforce/label/c.BI_PSP_OptionValueYes';
+export { default as NO_LABEL } from '@salesforce/label/c.BI_PSP_OptionValueNo';
+export { default as LETSPERSONLIZE_URL } from '@salesforce/label/c.BI_PSPB_LetsPersonalizeUrl';
+
+export { default as DLQI_CATEGORY } from '@salesforce/label/c.BI_PSP_DlqiCategory';
+export { default as DLQI_HEADING_TEXT } from '@salesforce/label/c.BI_PSP_DlqiHeadingOne';
+export { default as DLQI_HEADING_ONE_TEXT } from '@salesforce/label/c.BI_PSP_DlqiHeadingTwo';
+export { default as ANSWER_LABEL } from '@salesforce/label/c.BI_PSP_AnsweredLabel';
+export { default as DLQI_URL } from '@salesforce/label/c.BI_PSPB_DlqiQuestionnaireUrl';
+export { default as DLQI_COMPLETED_URL } from '@salesforce/label/c.BI_PSPB_DlqiCompletedUrl';
+
+export { default as WPAI_CATEGORY } from '@salesforce/label/c.BI_PSP_WapiCategory';
+export { default as WPAI_QUESTIONAIRE } from '@salesforce/label/c.BI_PSPB_WapiQuestionnaire';
+export { default as WPAI_COMPLETED_QUESTIONNAIRE } from '@salesforce/label/c.BI_PSPB_WapiCompletedQuestionnaire';
+
+export { default as QUALITATIVE_LABEL } from '@salesforce/label/c.BI_PSP_QualitativeCategory';
+export { default as QUALITATIVE_TWO_MONTHS } from '@salesforce/label/c.BI_PSPB_QualitativeTwoMonths';
+export { default as QUALITATIVE_FOURTEENWEEKS } from '@salesforce/label/c.BI_PSPB_QualitativeFourteenWeeks';
+export { default as QUALITATIVE_COMPLETED_FOURTEEN_WEEKS } from '@salesforce/label/c.BI_PSPB_QualitativeFourteenwksCompletedUrl';
+export { default as QUALITATIVE_COMPLETED_TWO_MONTHS } from '@salesforce/label/c.BI_PSPB_QualitativeTwoMonthsCompletedUrl';
+export { default as COMPLETE } from '@salesforce/label/c.BI_PSP_Complete';
+
+export { default as COMPLETED_QUESTIONNAIRE_TXT } from '@salesforce/label/c.BI_PSP_CompletedQuestionnaireTxt';
+export { default as ROLL_OUT_TXT } from '@salesforce/label/c.BI_PSP_RolloutDateTxt';
+export { default as EXPIRED_ON } from '@salesforce/label/c.BI_PSP_ExpiredOnLabel';
+export { default as DLQI_BOTTOM } from '@salesforce/label/c.BI_PSP_DlqiFirstBottomMsg';
+export { default as DLQI_BOTTOM_TEXT_TWO } from '@salesforce/label/c.BI_PSP_DlqiSecBottomMsg';
+export { default as DLQI_BOTTOM_TEXT_THREE } from '@salesforce/label/c.BI_PSP_PssBottomSecndMsg';
+export { default as BI_PSP_dlqibottom4 } from '@salesforce/label/c.BI_PSP_DlqiThreeBottomMsg';
+export { default as ERROR_LOADING } from '@salesforce/label/c.BI_PSP_ErrorInLoading';
+export { default as SELECT_MONTH } from '@salesforce/label/c.BI_PSP_SelectMonthQues';
+export { default as NO_ASSESSMENT_RECORD_FOUND } from '@salesforce/label/c.BI_PSP_NoAssessmentFound';
+
+
+
+export { default as WPAI_TXT } from '@salesforce/label/c.BI_PSP_WpaiQstnrTxt';
+export { default as PSS_BOTTOM_TXT } from '@salesforce/label/c.BI_PSP_PssBottomMsg';
+export { default as PSS_BOTTOM_TXT_ONE } from '@salesforce/label/c.BI_PSP_PssBottomSecndMsg';
+
+
+
+export { default as INFORMATION_CENTER_TXT } from '@salesforce/label/c.BI_PSP_InformationCentreTxt';
+export { default as SYMPTOM_TRACKER_TXT } from '@salesforce/label/c.BI_PSP_SymptomTrackerValue';
+export { default as CHALLENGES_TXT } from '@salesforce/label/c.BI_PSP_ChallengesName';
+export { default as QUESTIONNAIRE_TXT } from '@salesforce/label/c.BI_PSP_Questionnaire';
+export { default as TREATMENT_VIDEOS } from '@salesforce/label/c.BI_PSP_TreatmentVideosTxt';
+export { default as SUPPORT_TXT } from '@salesforce/label/c.BI_PSP_SupportTxt';
+export { default as QSQ_EG_TXT } from '@salesforce/label/c.BI_PSP_QualitativeEgTxt';
+
+
+
+export { default as OUT_STANDING_URL } from '@salesforce/label/c.BI_PSPB_OutstndngPageUrl';
+export { default as SUMMARY_URL } from '@salesforce/label/c.BI_PSPB_SummaryUrl';
+export { default as CHRONIC_STATUS } from '@salesforce/label/c.BI_PSPB_ChronicStatus';
+export { default as ACUTE_STATUS } from '@salesforce/label/c.BI_PSP_Acute';
+export { default as UNASSIGNED_STATUS } from '@salesforce/label/c.BI_PSP_Unassigned';
+
+
+
+export { default as WPAI_BOTTOM_TXT } from '@salesforce/label/c.BI_PSP_WpaiBottomTxt';
+export { default as WPAI_BOTTOM_TXT_TWO } from '@salesforce/label/c.BI_PSP_PssBottomSecndMsg';
+export { default as INCLUDE_TXT } from '@salesforce/label/c.BI_PSP_WpaiIncludeTxt';
+export { default as HOUR_TXT } from '@salesforce/label/c.BI_PSP_HoursForChatter';
+export { default as WPAI_SKIP } from '@salesforce/label/c.BI_PSP_WpaiSkipTxt';
+export { default as WPAI_SEC_TXT } from '@salesforce/label/c.BI_PSP_WpaiSecTxt';
+export { default as WPAI_SLIDER_TWO } from '@salesforce/label/c.BI_PSP_WpaiSliderSecond';
+export { default as WPAI_DAILY_LEFT } from '@salesforce/label/c.BI_PSP_WpaiDailyLeft';
+export { default as WPAI_DAILY_RIGHT } from '@salesforce/label/c.BI_PSP_WpaiDailyRight';
+export { default as WPAI_WORK_LEFT } from '@salesforce/label/c.BI_PSP_WpaiWorkLeft';
+export { default as WPAI_WORK_RIGHT } from '@salesforce/label/c.BI_PSP_WpaiWorkRight';
+export { default as VERY_MUCH } from '@salesforce/label/c.BI_PSP_RbVeryMuch';
+export { default as A_LOT } from '@salesforce/label/c.BI_PSP_RbAlot';
+export { default as A_LITTLE } from '@salesforce/label/c.BI_PSP_RbAlittle';
+export { default as NOT_AT_ALL } from '@salesforce/label/c.BI_PSP_RbNotAtAll';
+export { default as NOT_RELEVANT } from '@salesforce/label/c.BI_PSP_RbNotRelevant';
+export { default as ANSWERED } from '@salesforce/label/c.BI_PSP_NumOfAnsrdQstn';
+export { default as SUBMIT } from '@salesforce/label/c.BI_PSP_ButtonSubmit';
+export { default as SAVE_AS_DRAFT } from '@salesforce/label/c.BI_PSP_DraftButton';
+export { default as ROLLOUT_DATE } from '@salesforce/label/c.BI_PSP_QuestnrRollOutDate';
+export { default as EXPIRES_ON } from '@salesforce/label/c.BI_PSP_QuestnrExpiresOn';
+export { default as OUTSTANDING_QUESTIONNAIRE } from '@salesforce/label/c.BI_PSP_OutstndngPageTxt';
+export { default as BUTTON_RETURN_BACK } from '@salesforce/label/c.BI_PSP_ButtonReturnback';
+export { default as BUTTON_CONFIRM_SUB } from '@salesforce/label/c.BI_PSP_ButtonConfirmSub';
+export { default as CANCEL_BUTTON } from '@salesforce/label/c.BI_PSP_CancelButton';
+export { default as CONFIRM_BUTTON } from '@salesforce/label/c.BI_PSP_ConfirmButton';
+export { default as POPUP_MESSAGE } from '@salesforce/label/c.BI_PSP_MsgPopupTxt';
+export { default as SUBMIT_LABEL } from '@salesforce/label/c.BI_PSP_SubmitLabel';
+export { default as CANNOT_EDIT_MSG } from '@salesforce/label/c.BI_PSP_CannotEditMsg';
+export { default as COMPLETED_ALL } from '@salesforce/label/c.BI_PSP_CompleteAll';
+
+
+
+
+export { default as VERY_SEVERE } from '@salesforce/label/c.BI_PSP_RbVerySevere';
+export { default as SEVERE } from '@salesforce/label/c.BI_PSP_RbSevere';
+export { default as MODERATE } from '@salesforce/label/c.BI_PSP_RbModerate';
+export { default as MILD } from '@salesforce/label/c.BI_PSP_RbMild';
+export { default as NONE } from '@salesforce/label/c.BI_PSP_RbNone';
+export { default as PSS_BOTTOM_SEC_MSG } from '@salesforce/label/c.BI_PSP_PssBottomSecndMsg';
+
+export { default as WPAI_ALERT_TEXT } from '@salesforce/label/c.BI_PSP_WpaiAlertText';
+export { default as WPAI_SLIDER_QUEST_TEXT } from '@salesforce/label/c.BI_PSP_WpaiSliderQuestText';
+export { default as WORKING_FOR_PAY_TEXT } from '@salesforce/label/c.BI_PSP_WorkingForPayText';
+export { default as CLICK_BELOW } from '@salesforce/label/c.BI_PSP_ClickBelowText';
+
+export { default as INTROPAGEONEBOTTOM } from '@salesforce/label/c.BI_PSP_IntroBottomTxt';
+
+
+export { default as LESSTHAN_A_MONTH } from '@salesforce/label/c.BI_PSP_RbOneMonth';
+export { default as SIXMONTHS } from '@salesforce/label/c.BI_PSP_RbSixMnth';
+export { default as LESS_THAN_YEAR } from '@salesforce/label/c.BI_PSP_RbOneYear';
+export { default as MORE_THAN_YEAR } from '@salesforce/label/c.BI_PSP_RbOneYearMore';
+export { default as MALE } from '@salesforce/label/c.BI_PSP_RbMale';
+export { default as FEMALE } from '@salesforce/label/c.BI_PSP_RbFemale';
+export { default as OTHER } from '@salesforce/label/c.BI_PSP_RbOther';
+export { default as PREFERNOT_TOSAY } from '@salesforce/label/c.BI_PSP_RbNotToSay';
+
+export { default as RELATIONSHIPWITH_FF } from '@salesforce/label/c.BI_PSP_RbFamAndFriends';
+export { default as RELATIONSHIPWITH_PARTNER } from '@salesforce/label/c.BI_PSP_RbWithPartner';
+export { default as SELF_ESTEEM } from '@salesforce/label/c.BI_PSP_RbSelfEsteem';
+export { default as SELECT_ALL } from '@salesforce/label/c.BI_PSP_RbSelectAll';
+export { default as ASTHMA } from '@salesforce/label/c.BI_PSP_RbAsthma';
+export { default as DIABETESMELLITUS } from '@salesforce/label/c.BI_PSP_RbDiabetesMellitus';
+export { default as DEPRESSION } from '@salesforce/label/c.BI_PSP_RbDepression';
+export { default as HAY_FEVER } from '@salesforce/label/c.BI_PSP_RbHayFever';
+export { default as HYPERTENSION } from '@salesforce/label/c.BI_PSP_RbHyperTension';
+export { default as HIGHCHOLESTEROL } from '@salesforce/label/c.BI_PSP_RbHighCholesterol';
+export { default as OBESITY } from '@salesforce/label/c.BI_PSP_Obesity';
+export { default as OSTEOPOROSIS } from '@salesforce/label/c.BI_PSP_Osteoporosis';
+export { default as ULCER } from '@salesforce/label/c.BI_PSP_Ulcer';
+export { default as PSORIASIS } from '@salesforce/label/c.BI_PSP_RbPsoriasis';
+export { default as PSORIATICARTHRITIS } from '@salesforce/label/c.BI_PSP_PsoriaticArthritis';
+export { default as DO_YOU_AGREE } from '@salesforce/label/c.BI_PSP_QstnDoYouAgree';
+export { default as FIFTH_QUESTION } from '@salesforce/label/c.BI_PSP_FifthQstnTxtIntro';
+export { default as SIXTH_QUESTION } from '@salesforce/label/c.BI_PSP_SixthQstnTxtIntro';
+export { default as MAYBE } from '@salesforce/label/c.BI_PSP_RbMaybe';
+
+
+export { default as CANNOT_BE_EDITED } from '@salesforce/label/c.BI_PSP_CannotBeEditedTxt';
+export { default as SUBMIT_MESSAGE } from '@salesforce/label/c.BI_PSP_SubmitLabel';
+export { default as OTHERS } from '@salesforce/label/c.BI_PSP_Others';
+export { default as ERROR_PAGE } from '@salesforce/label/c.BI_PSP_DisplayNavErrorPage';
+
+
+
+
+export { default as MYSELF } from '@salesforce/label/c.BI_PSP_IntroAboutMyself';
+export { default as NEXT } from '@salesforce/label/c.BI_PSP_NextButton';
+export { default as SKIP } from '@salesforce/label/c.BI_PSP_SkipButton';
+
+
+
+
+
+export { default as PERZONALISE } from '@salesforce/label/c.BI_PSPB_LetsPersonalisePageTwo';
